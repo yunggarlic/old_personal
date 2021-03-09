4d7c2f601d31fd9dc43ec270e2e9d039d5f581d5
yunggarlic <ferrari.tim.v@gmail.com>
2021-03-08 23:43:34 -0600

    deletes cute hello because history switch success

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Scene, Navbar, Navbox } from './components';
 import { Box, Container } from '@material-ui/core';
 import useStyles from './styles/material-styles';
-//hello
 
 export default function Home() {
   const { main, content } = useStyles();
