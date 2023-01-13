7d11e16c14b63533e135824a04a49c86ca2fc6c3
Tim Ferrari <ferrari.tim.v@gmail.com>
2023-01-12 21:53:18 -0600

    updates welcome text remount

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,31 @@
-import React, { useEffect } from 'react';
-import '../styles/home.less';
+import React, {useEffect} from 'react'
+import '../styles/home.less'
 
 export default function Home(props) {
-  const { isFocus, onMobile, isFirstTime } = props;
+  const {isFocus, onMobile, isFirstTime} = props
 
-  useEffect(()=> {console.log(isFirstTime)}, [isFirstTime])
+  useEffect(() => {}, [isFirstTime])
 
   return (
     // Must fade in and out on is focus
     <div className={`home ${isFocus === 'home' ? 'active' : ''}`}>
-      {isFirstTime ? <h1 className='text-rainbow'>Hello.</h1> : <h1 className='text-rainbow'>Welcome back.</h1>}
+      <h1 className="text-rainbow">
+        {isFirstTime ? 'Hello.' : 'Welcome Back'}
+      </h1>
+      <div className={`buttonContainer ${isFirstTime ? '' : 'active'}`}>
+        <button>
+          <a href="http://www.linkedin.com/in/timferrari">Connect with me!</a>
+        </button>
+      </div>
       {onMobile ? (
         <p>
-          Quite honestly this will probably crash on your phone eventually.<br/>But
-          all info is still available.
+          Quite honestly this will probably crash on your phone eventually.
+          <br />
+          But all info is still available.
         </p>
       ) : (
         <></>
       )}
     </div>
-  );
+  )
 }
