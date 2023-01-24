07baa841f322da092657ddddfbd9fb06f708f39f
yunggarlic <ferrari.tim.v@gmail.com>
2023-01-23 21:20:54 -0600

    adds svg border, updates nav

--- src/components/Simple.js
+++ src/components/Simple.js
@@ -4,7 +4,7 @@ import Cursor from './SimpleComponents/Cursor';
 
 const Simple = () => {
   const [isFocus, setIsFocus] = useState('home');
-  const sections = ['home', 'about', 'work'];
+  const sections = ['home', 'fun', 'work'];
 
   //set active class on navbar buttons
   const handleClick = (e) => {
@@ -15,16 +15,22 @@ const Simple = () => {
     e.target.classList.add('active');
 
     const activeSection = document.querySelector('.content > div.active');
+
+    //Initiate outbound transition
     activeSection.classList.remove('active');
+
+    //Transition lasts 200ms, then dismount component
     setTimeout(() => {
       setIsFocus('');
     }, 200);
+
+    //Mount new component and initiate inbound transition
     setTimeout(() => {
       setIsFocus(e.target.innerText.toLowerCase());
+      console.log(isFocus);
       const sections = document.querySelectorAll('.content > div');
       sections.forEach((section) => {
         section.classList.remove('active');
-        console.log(section.id);
         if (section.id === e.target.innerText.toLowerCase()) {
           section.classList.add('active');
         }
@@ -35,16 +41,18 @@ const Simple = () => {
   useEffect(() => {
     setTimeout(() => {
       const home = document.querySelector('#home');
+      const navbar = document.querySelector('#navbar');
       home.classList.add('active');
-    }, 200);
+      navbar.classList.add('active');
+    }, 1000);
   }, []);
 
   return (
     <div className="main">
       <div id="navbar">
-        {sections.map((section) => {
+        {sections.map((section, i) => {
           return (
-            <div className="buttonContainer">
+            <div id={i} className="buttonContainer">
               <button onClick={handleClick}>{section}</button>
             </div>
           );
@@ -56,10 +64,13 @@ const Simple = () => {
             <h1>Hello</h1>
             <p>Thanks for visiting my website.</p>
             <p>
-              I am Tim Ferrari. I am a software engineer, artist, musician,
-              elaborate worm, and aspiring polymath with a passion for creating
-              beautiful, intuitive, and useful web applications. I have
-              experience across a wide range of technologies and languages.
+              I am Tim Ferrari - professionally a software engineer with a
+              passion for creating beautiful, intuitive, and useful web
+              applications.
+            </p>
+            <p>
+              And otherwise an artist, musician, elaborate worm, and aspiring
+              polymath.
             </p>
             <p>
               Currently, I am a Developer at{' '}
@@ -67,14 +78,32 @@ const Simple = () => {
                 Critical Mass
               </a>
               , and developing independently on a fullstack application for a
-              local startup. I am always looking for new opportunities to learn,
-              build, and have a laugh.
+              local startup. I am always looking for new opportunities to build.
             </p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+              <line class="top" x1="0" y1="0" x2="100%" y2="0"></line>
+              <line class="left" x1="0" y1="0" x2="0" y2="100%"></line>
+              <line class="bottom" x1="0" y1="100%" x2="100%" y2="100%"></line>
+              <line class="right" x1="100%" y1="0" x2="100%" y2="100%"></line>
+            </svg>
           </div>
         ) : (
           <></>
         )}
-        {isFocus === 'about' ? <div id="about"></div> : <> </>}
+        {isFocus === 'fun' ? (
+          <div id="fun">
+            <p>Seeing as you're on a non-mobile device...</p>
+            <p>Perhaps you'd like to see the other version of the site?</p>
+            <p>It's graphically demanding!</p>
+            <div class="buttonContainer">
+              <button>
+                <a href="/fun">Fun Time</a>
+              </button>
+            </div>
+          </div>
+        ) : (
+          <> </>
+        )}
         {isFocus === 'work' ? (
           <div id="work">
             <h2>Selected Works</h2>
